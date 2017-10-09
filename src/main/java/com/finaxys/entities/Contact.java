1392aa1d6a7be009f80bb67b2ad8e889df34b4fc
Nacim Kacel <nacimkacel@MacBook-Pro-de-Nacim.local>
2017-10-09 10:27:29 +0200

    Adding entities

---
+++ src/main/java/com/finaxys/entities/Contact.java
@@ -0,0 +1,41 @@
+package com.finaxys.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Contact implements Serializable {
+	@Id @GeneratedValue
+	private Long id;
+	private String nom;
+	private String prenom;
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
+	private String mail;
+	private Long tel;
+	private String photo;
+	
+	
+	public Contact(String nom, String prenom, Date dateNaissance, String mail, Long tel, String photo) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.mail = mail;
+		this.tel = tel;
+		this.photo = photo;
+	} 
+}
